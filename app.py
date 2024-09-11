89498f468bcfdcde18e889cc725b56f744162651
KosFur <konsfur@gmail.com>
2024-09-11 20:46:25 +0300

    My Pract

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
 from flask import Flask
 from random import choice
+from flask import request
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII']=False
 
 quotes = [
-    {'id': 1, 'quote' : 'First quote'},
-    {'id': 2, 'quote': 'Second quote.'},
-    {'id': 3, 'quote': 'Third quote.'},
-    {'id': 4, 'quote': 'Fifth quote.'}
+   {
+       "id": 3,
+       "author": "Rick Cook",
+       "text": "Программирование сегодня — это гонка разработчиков программ, стремящихся писать программы с большей и лучшей идиотоустойчивостью, и вселенной, которая пытается создать больше отборных идиотов. Пока вселенная побеждает."
+   },
+   {
+       "id": 5,
+       "author": "Waldi Ravens",
+       "text": "Программирование на С похоже на быстрые танцы на только что отполированном полу людей с острыми бритвами в руках."
+   },
+   {
+       "id": 6,
+       "author": "Mosher’s Law of Software Engineering",
+       "text": "Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили."
+   },
+   {
+       "id": 8,
+       "author": "Yoggi Berra",
+       "text": "В теории, теория и практика неразделимы. На практике это не так."
+   },
+
 ]
+
 
 about_me = {
    "name": "Константин",
@@ -26,7 +45,9 @@ def hello_world():
 def about():
    return about_me
 
-
+@app.route("/quotes")
+def get_quotes():
+   return quotes
 
 @app.route('/quotes/<int:quote_id>')
 def get_quote(quote_id):
@@ -48,6 +69,28 @@ def get_quote_count():
 def get_random_quote():
     random_quote = choice(quotes)
     return (random_quote)
+
+@app.route("/quotes", methods=['POST'])
+def create_quote():
+   data = request.json
+   if 'author' not in data or 'text' not in data:
+        return {"error": "Invalid data, 'author' and 'text' are required"}, 400
+   new_id = get_new_id()
+
+   new_quote = {
+        "id": new_id,
+        "author": data['author'],
+        "text": data['text']
+    }
+   quotes.append(new_quote)
+
+   return new_quote, 201
+
+def get_new_id():
+    if quotes:
+        return max(quote['id'] for quote in quotes) + 1
+    else:
+        return 1
 
 if __name__ == "__main__":
     app.run(debug=True)
