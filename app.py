a72c04c5ce544fc544a2e4a12d17d0cfabb11e4f
KosFur <konsfur@gmail.com>
2024-09-11 14:46:49 +0300

    My HW

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 app.config['JSON_AS_ASCII']=False
 
+quotes = [
+    {'id': 1, 'quote' : 'First quote'},
+    {'id': 2, 'quote': 'Second quote.'},
+    {'id': 3, 'quote': 'Third quote.'},
+    {'id': 4, 'quote': 'Fifth quote.'}
+]
+
 about_me = {
    "name": "Константин",
    "surname": "Фурманов",
@@ -17,6 +24,19 @@ def hello_world():
 @app.route("/about")
 def about():
    return about_me
+
+
+
+@app.route('/quotes/<int:quote_id>')
+def get_quote(quote_id):
+    
+    quote = next((quote for quote in quotes if quote['id'] == quote_id), None)
+
+    if quote:
+        return quote 
+    else:
+        
+      return f"Quote with id={quote_id} not found", 404
 
 if __name__ == "__main__":
     app.run(debug=True)
