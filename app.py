b5839ea68d9747934b3febe80c1146e6de6d213e
KosFur <konsfur@gmail.com>
2024-09-16 21:24:59 +0300

    My commit

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ from flask import Flask, jsonify, request
 from random import choice
 from http import HTTPStatus
 from pathlib import Path
-import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, func
+from sqlalchemy.exc import InvalidRequestError
+from flask_migrate import Migrate
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
+
+class Base(DeclarativeBase):
+    pass
 
 
 
@@ -16,28 +22,40 @@ BASE_DIR = Path(__file__).parent
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{BASE_DIR / 'main.db'}"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{BASE_DIR / 'quotes.db'}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
-class Base(DeclarativeBase):
-    pass
 
 
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
+migrate= Migrate(app, db)
 
-class QuoteModel(Base):
+class AuthorModel(db.Model):
+    __tablename__ = 'authors'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[int] = mapped_column(String(32), index= True, unique=True)
+    quotes: Mapped[list['QuoteModel']] = relationship(back_populates='author', lazy='dynamic')
+
+    def __init__(self, name):
+       self.name = name
+
+    def to_dict(self):
+        return {'name': self.name}
+
+
+class QuoteModel(db.Model):
     __tablename__ = 'quotes'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    author: Mapped[str] = mapped_column(String(32), nullable=False)
-    text: Mapped[str] = mapped_column(String(255), nullable=False)
-    rating: Mapped[int] = mapped_column(default=1, nullable=False) 
+    author_id: Mapped[str] = mapped_column(ForeignKey('authors.id'))
+    author: Mapped['AuthorModel'] = relationship(back_populates='quotes')
+    text: Mapped[str] = mapped_column(String(255))
 
     def __init__(self, author, text):
         self.author = author
         self.text  = text
+
 
 
 with app.app_context():
@@ -91,42 +109,66 @@ def quotes_count():
    return jsonify(count=len(quotes))
 
 
-@app.route("/quotes", methods=['POST'])
 def create_quote():
-   """ Функция создает новую цитату в списке."""
-   new_quote = request.json  
-   last_quote = quotes[-1] 
-   new_id = last_quote["id"] + 1
-   new_quote["id"] = new_id
-   rating = new_quote.get("rating")
-   if rating is None or rating not in range(1, 6):
-      new_quote["rating"] = 1
-   quotes.append(new_quote)
-   return jsonify(new_quote), 201
+    new_quote_data = request.json
+    author = new_quote_data.get("author")
+    text = new_quote_data.get("text")
+    rating = new_quote_data.get("rating", 1)
+    if not author or not text:
+        return jsonify({"error": "Author and text are required"}), 400
+    if rating not in range(1, 6):
+        return jsonify({"error": "Rating must be between 1 and 5"}), 400
+
+    new_quote = QuoteModel(author=author, text=text, rating=rating)
+    with app.app_context():
+        db.session.add(new_quote)
+        db.session.commit()
+    return jsonify({
+        "id": new_quote.id,
+        "author": new_quote.author,
+        "text": new_quote.text,
+        "rating": new_quote.rating
+    }), 201
 
 
 @app.route("/quotes/<int:quote_id>", methods=["PUT"])
 def edit_quote(quote_id):
-   new_data = request.json
-   if not set(new_data.keys()) - set(('author', 'rating', 'text')):
-      for quote in quotes:
-         if quote["id"] == quote_id:
-            if "rating" in new_data and new_data["rating"] not in range(1, 6):
-               new_data.pop("rating")
-            quote.update(new_data)
-            return jsonify(quote), HTTPStatus.OK
-   else:
-      return {"error": "Send bad data to update"}, HTTPStatus.BAD_REQUEST 
-   return {"error": f"Quote with id={quote_id} not found."}, 404
+    new_data = request.json
+    if not set(new_data.keys()) <= set(('author', 'rating', 'text')):
+        return {"error": "Send bad data to update"}, HTTPStatus.BAD_REQUEST
+    
+  
+    with app.app_context():
+        quote = db.session.get(QuoteModel, quote_id)
+        if quote is None:
+            return {"error": f"Quote with id={quote_id} not found."}, 404
+        
+   
+        if "rating" in new_data and (new_data["rating"] not in range(1, 6)):
+            new_data.pop("rating")  
+       
+        for key, value in new_data.items():
+            setattr(quote, key, value)
+        db.session.commit()
 
+    return jsonify({
+        "id": quote.id,
+        "author": quote.author,
+        "text": quote.text,
+        "rating": quote.rating
+    }), HTTPStatus.OK
 
 @app.route("/quotes/<int:quote_id>", methods=["DELETE"])
 def delete_quote(quote_id: int):
-   for quote in quotes:
-      if quote["id"] == quote_id:
-         quotes.remove(quote)
-         return jsonify({"message": f"Quote with id={quote_id} has deleted."}), 200 
-   return {"error": f"Quote with id={quote_id} not found."}, 404  
+    with app.app_context():
+        quote = db.session.get(QuoteModel, quote_id)
+        if quote is None:
+            return {"error": f"Quote with id={quote_id} not found."}, 404
+        
+        db.session.delete(quote)
+        db.session.commit()
+
+    return jsonify({"message": f"Quote with id={quote_id} has been deleted."}), 20
 
 
 @app.route("/quotes/filter")
